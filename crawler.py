4d6b5461d344054cf1974d17ac92fb553d722c62
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-05-02 14:11:41 -0400

    Removes excess print statements

--- crawler.py
+++ crawler.py
@@ -1,4 +1,3 @@
-
 import requests
 import HTMLParser
 from bs4 import BeautifulSoup
@@ -14,6 +13,7 @@ to_crawl = Queue()
 to_be_indexed = Queue(maxsize=100)
 
 search_index = {}
+link_list = {}
 
 print '--------'
 
@@ -21,12 +21,14 @@ def normalize(url):
     return url if url.endswith('/') else url + '/'
 
 def crawl():
+    """Gets a url from the list of urls to be crawled and puts """
     while True:
-        print "Queue size before {}".format(to_be_indexed.qsize())
         url = to_crawl.get()
         to_be_indexed.put((url,get_page_text(url)))
         print "\033[94m Finished crawling {0} \033[0m \n Queue Size {1}".format(url,to_be_indexed.qsize())
+
 def get_page_text(url):
+    """Handles html retrieval only"""
     try:
         r = requests.get(url,allow_redirects=False)
     except (requests.exceptions.ConnectionError, requests.exceptions.InvalidSchema):
@@ -42,6 +44,7 @@ def index(url,text):
         return
     for link in s.findAll('a'):
         abs_link_url = normalize(urlparse.urljoin(url, link.get('href')))
+        link_list.setdefault(url,[]).append(abs_link_url)
         if not abs_link_url in crawled_or_queued:
             crawled_or_queued.add(abs_link_url)
             to_crawl.put(abs_link_url)
@@ -51,20 +54,16 @@ def index(url,text):
     
     for word in set(words):
         search_index.setdefault(word, []).append(url)
-    print "Indexing Completed: {0}\n Queue Size {1}".format(url,to_be_indexed.qsize())
-
     
 def get_words_from_tag(tag):
      return [''.join(re.findall(r'[a-zA-Z]', x)).lower() for x in tag.getText().split()]
     
 
-
-
 if __name__ == '__main__':
     try:
-        crawled_or_queued.add(u'http://www.nytimes.com/')
-        to_crawl.put(u'http://www.nytimes.com/')
-        for i in range(100):
+        crawled_or_queued.add(u'http://www.racialicious.com/')
+        to_crawl.put(u'http://www.racialicious.com/')
+        for i in range(5):
             t = threading.Thread(target=crawl)
             t.daemon = True
             t.start()
@@ -74,11 +73,8 @@ if __name__ == '__main__':
             url, text = to_be_indexed.get()
             if text:
                 index(url,text)
-            print "Finished Index Attempt"
     except KeyboardInterrupt:
-        pass
-        # pprint(search_index)
-        # print search_index.keys()
+        pprint(link_list)
 
     while True:
         word = raw_input('enter search term!')
